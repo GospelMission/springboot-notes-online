ec5a2a1933c8e95b1688ac28a53f0377e53b541e
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-10-04 08:18:18 +0800

    first commit

---
+++ src/main/java/practicespringboot/notesapplicationonline/NotesApplicationOnlineApplication.java
@@ -0,0 +1,13 @@
+package practicespringboot.notesapplicationonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApplicationOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApplicationOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practicespringboot/notesapplicationonline/NotesApplicationOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package practicespringboot.notesapplicationonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
